43625c2449a5ff176e54ac5da9e0146b50fcf728
DennisEckerskorn <denniseckerskorn@gmail.com>
2025-04-14 12:32:24 +0200

    Add some files

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:admin123%40@localhost:5432/agent_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
